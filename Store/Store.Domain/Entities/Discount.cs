69872fefe46fd3edfa7c003867f9a56a2840d911
Thyago Bezerra <thyago.bezerra@prolins.com.br>
2020-03-10 11:22:31 -0300

    adicionando tests

--- Store/Store.Domain/Entities/Discount.cs
+++ Store/Store.Domain/Entities/Discount.cs
@@ -3,6 +3,12 @@ namespace Store.Domain.Entities
 {
     public class Discount : Entity
     {
+        public Discount(decimal amount, DateTime expireDate)
+        {
+            Amount = amount;
+            ExpireDate = expireDate;
+        }
+
         public decimal Amount { get; private set; }
         public DateTime ExpireDate { get; private set; }
 
---
+++ Store/Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,92 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Entities;
+using Store.Domain.Enums;
+
+namespace Store.Tests.Domain
+{
+    [TestClass]
+    public class OrderTests
+    {
+        private readonly Customer _customer = new Customer("André Baltieri", "andre@balta.io");
+        private readonly Product _product = new Product("Produto 1", 1, true);
+        private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
+        {
+            
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(8, order.Number.Length);
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
+        {
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pagamento_do_pedido_seu_status_deve_ser_aguardando_entrega()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pedido_cancelado_seu_status_deve_ser_cancelado()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_item_com_quantidade_zero_ou_menor_o_mesmo_nao_deve_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_pedido_valido_seu_total_deve_ser_50()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_desconto_expirado_o_valor_do_pedido_deve_ser_60()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_desconto_de_10_o_valor_do_pedido_deve_ser_50()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dada_uma_taxa_de_entrega_de_10_o_valor_do_pedido_deve_ser_60()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pedido_sem_cliente_o_mesmo_deve_ser_invalido()
+        {
+            Assert.Fail();
+        }
+    }
+}
